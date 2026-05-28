efc44da0ee0543e18cd1db89d5ed6d68cd656c50
LACHIE KEANE <lachlankeane014@gmail.com>
2026-05-28 16:40:20 +0200

    Imporved custom tests

--- mytests.pl
+++ mytests.pl
@@ -6,6 +6,12 @@ puzzle(no_diagonals, [1, 1, 1], [1, 1, 1],
      [-1,  2, -1],
      [-1, -1,  1]]).
 
+puzzle(no_diagonals2, [-1, -1, -1, -1], [-1, -1, -1, -1],
+    [[-1,  0,  2,  0],
+     [-1,  2, -1,  2],
+     [ 0,  2, -1,  2],
+     [ 1,  0, -1,  1]]).
+
 % Solve puzzles with pre-filled obstacles
 puzzle(obstacles, [-1, -1, -1, -1], [-1, -1, -1, -1],
     [[ 1, -1,  0,  1],
@@ -23,8 +29,8 @@ puzzle(prefilled, [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1],
 
 % Testing the 'no squares' rule. Should fail
 puzzle(no_squares, [2, 2], [2, 2],
-    [[-1,  1],
-     [ 1, -1]]).
+    [[-1, -1],
+     [ 1,  1]]).
 
 % Disconnected endpoints, should fail
 puzzle(disconnected_endpoints, [-1, -1, -1], [-1, 0, -1],
@@ -42,17 +48,17 @@ puzzle(challenge, [3, 4, -1, -1, -1, -1, 5], [-1, -1, -1, -1, 5, 4, -1],
      [-1, -1, -1, -1,  0, -1, -1],
      [-1, -1, -1, -1, -1, -1,  1]]).
 
-
-
-%% Shows all solutions for a single puzzle (REMEMBER TO TAKE THIS OUT BEFORE SUBMITTING)
-showAllSols(P) :-
-    format("~n=== ~w ===~n", [P]),
-    puzzle(P, RowClues, ColClues, Grid),
-    time(findall(Sol, snake(RowClues, ColClues, Grid, Sol), Solutions)),
-    ( Solutions = []
-    -> format(">>> ~w: NO solution found~n", [P])
-    ;  length(Solutions, N),
-       format(">>> ~w: ~w solution(s) found~n", [P, N]),
-       forall(member(Sol, Solutions),
-              print_puzzle(RowClues, ColClues, Sol))
-    ).
+% More difficult challenge, 12x12
+puzzle(challenge2, [3, -1, -1, -1, -1, -1, 5, -1, -1, -1, 8, 6], [3, 4, 5, 6, -1, 1, -1, 6, 2, 7, -1, -1],
+    [[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  2],
+     [ 0,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+     [-1, -1, -1, -1,  0,  0, -1, -1, -1, -1, -1, -1],
+     [-1, -1, -1, -1, -1, -1, -1, -1,  2, -1, -1, -1],
+     [-1, -1, -1,  2, -1, -1, -1, -1, -1, -1, -1, -1],
+     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0, -1],
+     [-1, -1, -1, -1, -1, -1,  2, -1, -1, -1,  2,  2],
+     [-1, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1, -1],
+     [-1, -1,  0, -1, -1, -1, -1, -1, -1,  2, -1, -1],
+     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+     [-1, -1, -1, -1, -1,  2, -1, -1, -1, -1, -1, -1],
+     [-1, -1,  2,  2, -1, -1, -1,  0,  0, -1, -1,  1]]).
